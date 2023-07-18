f11d4fdaca8fec4630685982316ca2ca1c9da155
moatazmezdari <moatazmezdari@gmail.com>
2023-07-18 16:39:37 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry Point.use only user defined variable to create outpout.
+ *
+ * Description: Print _putchar using predefined functions
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
